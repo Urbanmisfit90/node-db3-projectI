77208cb60265e8d5c479b098160ef01124c2a7a2
Robert Dixon <robmsc24@gmail.com>
2024-04-09 00:34:41 -0400

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,4 +1,4 @@
-const db = require('../../data/db-config')
+const db = require("../../data/db-config");
 /*
   If `scheme_id` does not exist in the database:
 
@@ -9,22 +9,22 @@ const db = require('../../data/db-config')
 */
 const checkSchemeId = async (req, res, next) => {
   try {
-    const existing = await db('schemes')
-    .where('scheme_id', req.params.scheme_id)
-    .first()
+    const existing = await db("schemes")
+      .where("scheme_id", req.params.scheme_id)
+      .first();
 
     if (!existing) {
       next({
         status: 404,
-        messsage: `scheme with scheme_id ${req.params.scheme_id} not found`
-      })
+        messsage: `scheme with scheme_id ${req.params.scheme_id} not found`,
+      });
     } else {
-      next()
+      next();
     }
-  } catch(err) {
-    next(err)
+  } catch (err) {
+    next(err);
   }
-}
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -35,8 +35,17 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  const { scheme_name } = req.body;
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== "string" ||
+    !scheme_name.trim()
+  ) {
+    next({ status: 400, message: "invalid scheme_name" });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -47,12 +56,10 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = (req, res, next) => {};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
